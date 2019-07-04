ccb62d4aa77d1f92b8848df50e08eee449d53a64
Lura Landon <lura.landon@gmail.com>
2019-07-04 04:46:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,5 +6,10 @@ def batch_badge_creator(attendees)
   attendees.collect{|attendee| badge_maker(attendee)}
 end
 
-
-
+def assign_rooms(attendees)
+  assigned_rooms = []
+  attendees.each_with_index do |attendee, index|
+    assigned_rooms << "Hello #{attendee}! You'll be assigned to room #{index +1}!"
+  end
+  assigned_rooms
+end
